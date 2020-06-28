0bf78598fb47ce31b97cd1abbffca907b0893c9c
geri <geriadam1@gmail.com>
2020-06-28 15:40:36 +0700

    init commit

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import WbnPlayer from './WbnPlayer';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import GlobalStyle from '../styles/GlobalStyle';
 
 const App = () => (
-  <BrowserRouter basename="/react_videoplayer/">
-  <>
-    <Switch>
-      <Route exact path="/" component={WbnPlayer} />
-      <Route exact path="/:activeVideo" component={WbnPlayer} />
-    </Switch>
-    <GlobalStyle />
-  </>
-  </BrowserRouter>
+  <div>Start here!</div>
 )
 
 export default App;
